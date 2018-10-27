c1609d2eea7e9918ae07cf3994a64d6f286e0218
Jorge Aparicio <jorge@japaric.io>
2018-10-28 00:18:13 +0200

    run objcopy test only on Linux

--- ci/script.sh
+++ ci/script.sh
@@ -5,7 +5,9 @@ main() {
 
     cargo nm --bin cargo-nm -v > /dev/null
     cargo objdump --bin cargo-objdump -v -- -d > /dev/null
-    cargo objcopy --bin cargo-objdump -v -- -O binary objdump.bin > /dev/null
+    if [ $TRAVIS_OS_NAME = linux ]; then
+        cargo objcopy --bin cargo-objdump -v -- -O binary objdump.bin > /dev/null
+    fi
     cargo size --bin cargo-size -v
     cargo strip --bin cargo-strip -v
 }
